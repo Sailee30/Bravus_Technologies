6ee565e42fd3a90bd80ce8c5cbf362bd23799e66
Sailee30 <saileechavan04@gmail.com>
2025-11-12 14:54:00 +0530

    Blogs page

---
+++ app/blogs/page.tsx
@@ -0,0 +1,9 @@
+export default function BlogsPage() {
+  return (
+    <div className="min-h-screen bg-black text-white">
+      <div className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-12 py-24">
+        <h1 className="text-4xl font-bold">Blogs</h1>
+      </div>
+    </div>
+  )
+}
